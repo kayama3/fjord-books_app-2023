74919083be66085f47412ceef18475490f93680a
kayama <shimonk333@gmail.com>
2023-03-07 23:39:01 +0900

    追加:letter_opener_webを追加
    送信したメールをブラウザ上で確認できるようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Defines the root path route ("/")
   # root "articles#index"
 end
