655ccea9124086e000fae7b23a9b3acef65f1168
kayama <shimonk333@gmail.com>
2023-03-10 09:23:02 +0900

    修正:ルーティングの仕方を変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope "(:locale)", locale: /en|ja/ do
-    resources :books
-  end
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
