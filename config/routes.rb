6b8d4a5b438fa0f2c1dd6bd95d4e445507486028
kayama <shimonk333@gmail.com>
2023-03-14 14:05:47 +0900

    追加:ユーザー一覧画面と、詳細画面を追加しました
    ユーザーテーブルに項目を追加し、必要な画面を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :books
+  get 'users/show'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :books, :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Defines the root path route ("/")
