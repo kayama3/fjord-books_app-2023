3415f2cddd92b6ee3671703724d27e8372d69eaa
kayama <shimonk333@gmail.com>
2023-03-17 18:17:27 +0900

    追加:ActiveStorageを追加しました
    ActiveStorageでユーザーのアイコン画像を追加できるようにしました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit]
+
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_icon.order(:id).page(params[:page])
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
