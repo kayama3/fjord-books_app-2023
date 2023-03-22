45fb454b46e88d0842ae312a062b18b37669f517
kayama <shimonk333@gmail.com>
2023-03-22 21:55:01 +0900

    修正:viewファイルとrouteファイルを修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :books, :users
+  resources :books
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Defines the root path route ("/")
