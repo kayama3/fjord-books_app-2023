e068e5fb82cbeb1f7ae245fc254300a9627b2ebf
kayama <shimonk333@gmail.com>
2023-04-13 20:10:08 +0900

    追加：ルーティングを追記
    books、reports、commentsのルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  resources :books do
+    resources :comments, only: %i[create destroy], module: :books
+  end
+
+  resources :reports do
+    resources :comments, only: %i[create destroy], module: :reports
+  end
+  
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i(index show)
 end
