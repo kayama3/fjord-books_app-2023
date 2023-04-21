547238e0e2f30fbeb6653baab1af072941386f99
kayama <shimonk333@gmail.com>
2023-04-21 17:46:56 +0900

    追加：N:Nの自己参照の関係を設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -14,4 +14,10 @@ class Report < ApplicationRecord
   def created_on
     created_at.to_date
   end
+
+  has_many :mentioning_relations, class_name: 'Mention', foreign_key: :mentioning_report_id
+  has_many :mentioning_reports, through: :mentioning_relations, source: :mentioned_report
+
+  has_many :mentioned_relations, class_name: 'Mention', foreign_key: :mentioned_report_id
+  has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report
 end
