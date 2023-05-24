4cc8dcd373c8e122300209ee93442b7ef97560d0
kayama <shimonk333@gmail.com>
2023-05-24 19:48:09 +0900

    修正：指摘箇所の修正
    create_report_mentionsとupdate_report_mentionsが明示的にtrue/falseを返すように修正
    バリデーションエラーが無視されないように修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -21,26 +21,38 @@ class Report < ApplicationRecord
   has_many :mentioned_relations, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report, dependent: :destroy
 
-  def mentioning_report_ids
-    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
+  def find_report_ids
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i).uniq
+  end
+
+  def create_new_mentions(ids)
+    ids.map do |report_id|
+      mentioning_relations.new(mentioned_report_id: report_id)
+    end
   end
 
   def create_report_mentions
-    save
+    return unless save!
 
-    mentioning_report_ids.each do |report_id|
-      mentioning_relations.create(mentioned_report_id: report_id)
+    report_ids = find_report_ids
+
+    transaction do
+      new_mentions = create_new_mentions(report_ids)
+      new_mentions.each(&:save!)
     end
   end
 
   def update_report_mentions(params)
-    update(params)
+    return unless update!(params)
 
-    mentioning_report_ids.each do |report_id|
-      mentioning_relations.create(mentioned_report_id: report_id)
+    report_ids = find_report_ids
+    mentioned_report_ids = report_ids - mentioning_reports.map(&:id)
+    unmentioned_reports = mentioning_reports.map(&:id) - report_ids
+
+    transaction do
+      new_mentions = create_new_mentions(mentioned_report_ids)
+      new_mentions.each(&:save!)
+      mentioning_relations.where(mentioned_report_id: unmentioned_reports).map(&:destroy!)
     end
-
-    unmentioned_report_ids = mentioning_reports.map(&:id) - mentioning_report_ids
-    mentioning_relations.where(mentioned_report_id: unmentioned_report_ids).map(&:destroy)
   end
 end
