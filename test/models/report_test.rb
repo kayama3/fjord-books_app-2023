e759dfd0bd20b47ea20cdaa52ac2959360ac72c8
kayama <shimonk333@gmail.com>
2023-06-09 20:18:56 +0900

    追加：modelテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "editable?" do
+    user = User.create!(email: 'hoge@example.com', password: 'password')
+    report = user.reports.create!(title:'hogehoge',content:'fugafuga')
+    assert report.editable?(user)
+  end
+
+  test "created_on" do
+    user = User.create!(email: 'hoge@example.com', password: 'password')
+    report = user.reports.create!(title:'hogehoge',content:'fugafuga')
+    assert_equal Time.current.to_date, report.created_on
+    assert true
+  end
 end
