b4c40316492a619634b599c4ac48fad6678419fe
kayama <shimonk333@gmail.com>
2023-06-14 20:48:20 +0900

    修正：細かな変更を２点
    各test名のアンダースコアをスペースに変更
    テストデータを実際のものに近いように変更

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,20 +3,23 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  test 'i18n_pluralize' do
-    # 微妙かも
-    # locale == :ja 前提でテスト書いてるけど、これじゃいかんか
+  test 'i18n pluralize' do
+    I18n.default_locale = :ja
     assert_equal '本', i18n_pluralize(Book.model_name.human.downcase)
-    assert_not_equal 'books', i18n_pluralize(Book.model_name.human.downcase)
+
+    I18n.default_locale = :en
+    assert_equal 'books', i18n_pluralize(Book.model_name.human.downcase)
   end
 
-  test 'i18n_error_count' do
-    # 上に同じく微妙そう
+  test 'i18n error ount' do
+    I18n.default_locale = :ja
     assert_equal '3件のエラー', i18n_error_count(3)
-    assert_not_equal '3 errors', i18n_error_count(3)
+
+    I18n.default_locale = :en
+    assert_equal '3 errors', i18n_error_count(3)
   end
 
-  test 'format_content' do
+  test 'format content' do
     content = "こんにちわ。\nいい天気ですね。\n今日の予定はなんですか。"
     assert_equal 'こんにちわ。<br>いい天気ですね。<br>今日の予定はなんですか。', format_content(content)
   end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,14 +5,13 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test 'editable?' do
     user = User.create!(email: 'hoge@example.com', password: 'password')
-    report = user.reports.create!(title: 'hogehoge', content: 'fugafuga')
+    report = user.reports.create!(title: '今日の日報', content: '今日も頑張った')
     assert report.editable?(user)
   end
 
-  test 'created_on' do
+  test 'created on' do
     user = User.create!(email: 'hoge@example.com', password: 'password')
-    report = user.reports.create!(title: 'hogehoge', content: 'fugafuga')
+    report = user.reports.create!(title: '今日の日報', content: '今日も頑張った')
     assert_equal Time.current.to_date, report.created_on
-    assert true
   end
 end
